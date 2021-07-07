171703c6a4b182710e955a2279a979993d1c5eea
Javoopls <ja.vivancoa@duocuc.cl>
2021-07-07 19:08:41 -0400

    viewslogin

--- rest/urls.py
+++ rest/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from rest.views import detalle_noticia, procesar_noticias
+from .views import detalle_noticia, procesar_noticias
+from .viewslogin import login
 
 urlpatterns = [
     path('noticias', procesar_noticias, name="procesar_noticias"),
-    path('noticias/<id>', detalle_noticia, name="detalle_noticia")
+    path('noticias/<id>', detalle_noticia, name="detalle_noticia"),
+    path('login',login, name='login')
 ]
